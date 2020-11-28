a73663593e39ebcf57b081b33320dc8ac2d5e108
Andrei Dracea <andrei.dracea1@gmail.com>
2020-11-28 21:58:58 +0200

    added home grid boxes + faq + carousel

--- frontend/src/buefy.js
+++ frontend/src/buefy.js
@@ -1,7 +1,14 @@
 import Vue from 'vue'
 
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faArrowRight, faArrowUp, faQuoteLeft } from '@fortawesome/free-solid-svg-icons'
+import {
+  faAngleLeft,
+  faAngleRight,
+  faArrowRight,
+  faArrowUp,
+  faQuoteLeft,
+  faQuestionCircle,
+} from '@fortawesome/free-solid-svg-icons'
 
 import {
   faFacebookSquare,
@@ -10,22 +17,33 @@ import {
 
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-library.add(faArrowRight, faArrowUp, faQuoteLeft, faFacebookSquare, faInstagram)
+library.add(
+  faAngleLeft,
+  faAngleRight,
+  faArrowRight,
+  faArrowUp,
+  faQuoteLeft,
+  faQuestionCircle,
+  faFacebookSquare,
+  faInstagram
+)
 Vue.component('vue-fontawesome', FontAwesomeIcon)
 
 import {
   Button,
   Carousel,
+  ConfigProgrammatic,
+  Collapse,
   Icon,
   Progress,
   Table,
-  Toast,
-  ConfigProgrammatic
+  Toast
 } from 'buefy'
 
 // Components
 Vue.use(Button)
 Vue.use(Carousel)
+Vue.use(Collapse)
 Vue.use(Icon)
 Vue.use(Progress)
 Vue.use(Table)
---
+++ frontend/src/components/HomeFAQ.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <b-collapse
+      class="question"
+      animation="slide"
+      v-for="(question, index) of data"
+      :key="index"
+      :open="isOpen == index"
+      @open="isOpen = index"
+    >
+      <a slot="trigger" class="question-header is-size-3 is-flex" role="button">
+        <b-icon icon="question-circle" class="has-text-primary" />
+        <span>{{ question.question }}</span>
+      </a>
+
+      <div class="content">
+        {{ question.answer }}
+      </div>
+    </b-collapse>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeFAQ',
+  components: {},
+  props: {
+    data: Array
+  },
+  data() {
+    return {
+      isOpen: null
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.question {
+  border-top: 1px solid $border;
+  padding: 20px 0;
+
+  .question-header {
+    color: $black;
+
+    .icon {
+      margin-right: 30px;
+    }
+
+    &:hover {
+      color: $primary;
+    }
+  }
+
+  .content {
+    padding: 12px 12px 0 50px;
+  }
+}
+</style>
