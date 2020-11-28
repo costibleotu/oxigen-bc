4bb6467131671d2c4c05067cef032fc06f5c21b4
Costin Bleotu <costi.bleotu@gmail.com>
2020-11-28 16:21:35 +0200

    added filters

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,5 +1,6 @@
 cd /root/oxigen_api
 git pull
 docker-compose -f production.yml build
-docker-compose -f production.yml restart
+docker-compose -f production.yml down
+docker-compose -f production.yml up -d
 
